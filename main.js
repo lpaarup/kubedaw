64b736c3179ee52624ce753c3f99df994f50251c
Luis Paarup <lpaarup@gmail.com>
2021-08-05 20:23:32 +0200

    changed to use banjo-sound

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-// var net = require('net');
-const Speaker = require('speaker');
-const Readable = require('stream').Readable
-const Buffer = require('buffer').Buffer
+const banjo = require('banjo-sound')
 
-const sine = new Readable()
-sine.bitDepth = 16
-sine.channels = 1
-sine.sampleRate = 44100
-sine.samplesGenerated = 0
-sine._read = read
-sine.pipe(new Speaker())
+b = new banjo.AudioStreamBuilder()
 
-let s = 0
-
-const freq = 800
-function read(n) {
-    const sampleSize = this.bitDepth / 8
-    const numSamples = n / (sampleSize * this.channels)
-    buf = Buffer.alloc(n)
-    const t = (Math.PI * 2 * freq) / this.sampleRate
-    for (let i = 0; i < numSamples; i++, s++) {
-        const val = Math.round(32760 * Math.sin(t * s))
-        for (let channel = 0; channel < this.channels; channel++) {
-            buf.writeInt16LE(val, i * 2 * this.channels + (channel * 2))
+class MyCallback extends banjo.AudioStreamDataCallback {
+    freq = 440
+    t = 0
+    async onAudioReady(stream, audioData, numFrames) {
+        const w = (Math.PI * 2 * this.freq) / stream.sampleRate
+        for (let frame = 0; frame < numFrames; frame++, this.t++) {
+            const val = Math.round(stream.maxAplitude * Math.sin(w * this.t))
+            for (let channel = 0; channel < stream.channelCount; channel++) {
+                audioData.writeInt16LE(val, frame * 2 * stream.channelCount + (channel * 2))
+            }
         }
     }
-
-    this.push(buf)
 }
+
+b.setDataCallback(new MyCallback())
+
+stream = b.newStream()
+if (stream.status != banjo.StreamStatus.OK) {
+    console.log("Failed to create stream. Error: " + stream.error)
+}
+
+/////////////////////// Option 1///////////////////////////////////////
+// requestStart and let the stream call onAudioReady when it needs data
+stream.requestStart()
